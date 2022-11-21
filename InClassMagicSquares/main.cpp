f3cdf0bcfd37bd01acb3fc6d02cd87a470dcd705
Chris Lane <lanec@pacificu.edu>
2022-11-21 07:58:44 -0800

    Initial Push

---
+++ InClassMagicSquares/main.cpp
@@ -0,0 +1,51 @@
+//**********************************************************************
+// File:       main.cpp
+// Author:     CS, Pacific University
+// Date:       11/21/22
+// Class:      CS 150-0x
+// Assignment: In class 2D Arrays
+// Purpose:    Gain more experience with 2D Arrays
+// Hours:
+//**********************************************************************
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+/***********************************************************************
+Function:     main
+Description:  Read in a square on integers from a file, print the square
+							to the screen in a variety of ways and check if square is
+							a magic sqaure. 
+Parameters:   None
+Returned:     Exit Status
+***********************************************************************/
+int main () {
+	const int SQUARE_SIZE = 3;
+
+	// TODO #1: Read the 3 x 3 square in square.txt into a 3 x 3 array
+
+
+
+	// TODO #2: Print the square to the screen.
+
+
+
+	// TODO #3: Print the square to the screen upside down (bottom row
+	//					first, then middle row, then top row)
+
+
+
+	// TODO #3: Print the square to the screen right to left (right 
+	//					column first, then middle column, then left column)
+
+
+
+	// TODO #4: Check to see if square is a Magic Square (the rows 
+	// 					and columns sum to 15
+
+
+
+	return EXIT_SUCCESS;
+}
